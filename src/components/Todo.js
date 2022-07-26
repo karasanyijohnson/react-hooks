b2a8653a194e611b171cc219e14f9b7b2f864e0e
Johnson <johnsonkarasanyi@gmail.com>
2022-07-26 12:55:44 +0200

    Add multiple states

---
+++ src/components/Todo.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const Todo = props => {
+    const [todoName, setTodoName] = useState('');
+    const [todoList, setTodoList] = useState([])
+    const inputChangeHandler = (event) => {
+        setTodoName(event.target.value)
+    }
+
+    const addTodoHundler = () => {
+        setTodoList(todoList.concat(todoName))
+    }
+    return <React.Fragment>
+        <input
+            type="text"
+            placeholder='Todo'
+            onChange={inputChangeHandler}
+            value={todoName}
+        />
+        <button type='button' onClick={addTodoHundler}>Add</button>
+        <ul>
+            {todoList.map(todo=>
+                <li key={todo}>{todo}</li>
+                ) }
+        </ul>
+    </React.Fragment>
+}
+
+export default Todo;
