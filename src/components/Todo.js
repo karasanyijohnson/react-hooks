e55d7995dcaa95e994b10850ecc3989f0aa10567
Johnson <johnsonkarasanyi@gmail.com>
2022-07-29 09:46:01 +0200

    useReducer Hook

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,41 +1,55 @@
-import React, { useState, useEffect, useReducer} from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios'
 
 const Todo = props => {
     const [todoName, setTodoName] = useState('');
-    const [todoList, setTodoList] = useState([])
-    const [submittedTodo, setSubmittedTodo]= useState(null)
+    const [submittedTodo, setSubmittedTodo] = useState(null)
+    // const [todoList, setTodoList] = useState([])
     // const [todoState, setTodoState] = useState({userInput:'', todoList:[]})
-    useEffect(()=>{
+
+    const todoListReducer = (state, action) => {
+        switch (action.type) {
+            case 'ADD':
+                return state.concat(action.payload)
+            case 'SET':
+                return action.payload    
+            case 'REMOVE':
+                return state.filter((todo) => todo.id !== action.payload)
+            default:
+                return state    
+        }
+    }
+    const [todoList, dispatch]= useReducer(todoListReducer,[])
+    useEffect(() => {
         axios.get('https://test-2f0e9-default-rtdb.firebaseio.com/todos.json')
-        .then(result=>{
-            console.log(result)
-            const todoData= result.data;
-            const todos =[]
-            for (const key in todoData){
-                todos.push({id:key, name: todoData[key].name})
-            }
-            setTodoList(todos)
-        })
-       return ()=>{
-        console.log("Clean Up")
-       }
-    },[])
-const mouseMoveHandler= event=>{
-    console.log(event.clientX, event.clientY)
-}
-    useEffect(()=>{
+            .then(result => {
+                console.log(result)
+                const todoData = result.data;
+                const todos = []
+                for (const key in todoData) {
+                    todos.push({ id: key, name: todoData[key].name })
+                }
+                dispatch({type:'SET', payload: todos})
+            })
+        return () => {
+            console.log("Clean Up")
+        }
+    }, [])
+    const mouseMoveHandler = event => {
+        console.log(event.clientX, event.clientY)
+    }
+    useEffect(() => {
         document.addEventListener("mousemove", mouseMoveHandler)
-        return ()=>{
+        return () => {
             document.removeEventListener('mousemove', mouseMoveHandler)
         }
-    },[])
+    }, [])
     useEffect(
-        ()=>{
-       if(submittedTodo){
-        setTodoList(todoList.concat(submittedTodo))
-       }
-    },[submittedTodo]);
+        () => {
+            if (submittedTodo) {
+                dispatch({type:'ADD', payload: submittedTodo})
+            }
+        }, [submittedTodo]);
     const inputChangeHandler = (event) => {
         setTodoName(event.target.value)
         // setTodoState({
@@ -45,17 +59,17 @@ const mouseMoveHandler= event=>{
     }
 
     const addTodoHundler = () => {
-        axios.post('https://test-2f0e9-default-rtdb.firebaseio.com/todos.json', {name: todoName})
-        .then(res=>{
-           setTimeout(()=>{
-            const todoItem = {id:res.data.name, name: todoName}
-            // setTodoList(todoList.concat(todoItem))
-            setSubmittedTodo(todoItem)
-           }, 3000)
-        })
-        .catch(err=>{
-            console.log(err)
-        })
+        axios.post('https://test-2f0e9-default-rtdb.firebaseio.com/todos.json', { name: todoName })
+            .then(res => {
+                setTimeout(() => {
+                    const todoItem = { id: res.data.name, name: todoName }
+                    // setTodoList(todoList.concat(todoItem))
+                    setSubmittedTodo(todoItem)
+                }, 3000)
+            })
+            .catch(err => {
+                console.log(err)
+            })
         // setTodoState({
         //     userInput:todoState.userInput,
         //    todoList:todoState.todoList.concat(todoState.userInput) 
@@ -67,14 +81,14 @@ const mouseMoveHandler= event=>{
             placeholder='Todo'
             onChange={inputChangeHandler}
             value={todoName}
-            // value={todoState.userInput}
+        // value={todoState.userInput}
         />
         <button type='button' onClick={addTodoHundler}>Add</button>
         <ul>
-            {todoList.map(todo=>
+            {todoList.map(todo =>
                 <li key={todo.id}>{todo.name}</li>
-                ) }
-                {/* {todoState.todoList.map(todo=>
+            )}
+            {/* {todoState.todoList.map(todo=>
                 <li key={todo}>{todo}</li>
                 ) } */}
         </ul>
